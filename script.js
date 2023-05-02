e9bf92cdea59b846487e8e739bba484b7986e5c6
Graehm <gpatrellfazio@gmail.com>
2023-05-01 19:00:16 -0500

    rendering the board pt1

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log("saved?")
+	// /*----- constants -----*/
+const PLAYERS {
+    'null': 'black',
+    '1': 'X',
+    '-1': 'O'
+}
+const winningCombos =
+[]
+
+	// /*----- state variables -----*/
+
+
+	// /*----- cached elements  -----*/
+
+
+	// /*----- event listeners -----*/
+
+
+	// /*----- functions -----*/
