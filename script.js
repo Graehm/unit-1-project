8e4eddff951c49ba718baab04f6730778289bc1d
Graehm <gpatrellfazio@gmail.com>
2023-05-01 20:30:16 -0500

    board rendered pt2 -- html/css mostly done. js constants and states

--- script.js
+++ script.js
@@ -4,11 +4,21 @@ const PLAYERS {
     '1': 'X',
     '-1': 'O'
 }
-const winningCombos =
-[]
+const winningCombos =[
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8], 
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
 
 	// /*----- state variables -----*/
-
+let board;
+let turn;
+let winner;
 
 	// /*----- cached elements  -----*/
 
