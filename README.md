86078706dc5d746cdefb27c95cc58c5d1767263b
Graehm <gpatrellfazio@gmail.com>
2023-05-06 17:49:02 -0500

    in the JS weeds-all questions are noted in muted code inside their functions

--- README.md
+++ README.md
@@ -2,7 +2,10 @@ May 1-3/4: complete html/css | js write what you can, pseudo the rest
 May 3/4-7: have JS working roughly 
 May 8-10: refine
 
-Perfect: 1. flex box to media query and shrink at certain size so it doesnt overlap with board. 
+Design touches: 1. flex box to media query and shrink at certain size so it doesnt overlap with board. 
 
-questions to ask: check JS files for embedded notes in code
+Js: --//single arr vs 2d arr. 1d easier to read for me, might convert code to 2d arr logic. 
+
+--// loop through winning combo array vs use 2d array 
+--// is itokay if forfeit and play computer are replacemebt for best of 3 out of 5
 
--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-	// /*----- constants -----*/
+// /*----- constants -----*/
 const PLAYERS = {
-    'null': 'black',
+    'null': 'white',
     '1': 'X',
     '-1': 'O'
 }
-const winningCombos =[
+const winningCombos = [
     [0, 1, 2],
     [3, 4, 5],
-    [6, 7, 8], 
+    [6, 7, 8],
     [0, 3, 6],
     [1, 4, 7],
     [2, 5, 8],
@@ -15,12 +15,12 @@ const winningCombos =[
     [2, 4, 6]
 ];
 
-	// /*----- state variables -----*/
-let turn = null; 
-let winner = null;
-let board =[];
-	
-    // /*----- cached elements  -----*/
+// /*----- state variables -----*/
+let turn;
+let winner;
+let board;
+
+// /*----- cached elements  -----*/
 // for winning message
 const winMessage = document.querySelector('h1');
 // to update score board --// how to select the two elements in flexbox. 
@@ -30,18 +30,21 @@ const forfeitBtn = document.querySelector('.quit');
 // play again
 const playAgainBtn = document.querySelector('.play');
 // for clickable board
+//---Q: the ...document copies the arr
 const boardTiles = [...document.querySelectorAll('#board > div')];
 
 
 
-	// /*----- event listeners -----*/
+// /*----- event listeners -----*/
 document.getElementById('#board').addEventListener('click', move);
 playAgainBtn.addEventListener('click', init);
 forfeitBtn.addEventListener('click', init);
 
 
 
-	// /*----- functions -----*/
+// /*----- functions -----*/
+init();
+
 function init() {
     turn = 1;
     winner = null;
@@ -53,6 +56,83 @@ function init() {
     render();
 }
 
-//     function move() {
-//     make move
+// render the board to take updates 
+function board() {
+    board.forEach(function (sq, idx) {
+        const squares = document.getElementById(`sq-${idx}`);
+        squares.style.backgroundColor = PLAYERS[''];
+    })
+}
+
+// render scoring updates to flexbox items 
+//---Q: how to grab the elements for scoring
+//---Q: F counter to increase the points from 0-5 as games are won.
+function scoring(player, computer) {
+    const playerPoints = document.getElementsByClassName(`scoreBox > playerScore`);
+    playerPoints;
+    const computerPoints = document.getElementsByClassName(`scoreBox > computerScore`);
+    computerPoints;
+}
+
+//render winning message
+//---Q: how to write that computer or player are winners?
+function winningMessage() {
+    if (winner === 'tie') {
+        h6.innerHTML = "Tie Game!";
+    } else if (winner === player) {
+        //player > computer;
+        h6.innerHTML = "Man beats Machine!";
+    } else if (winner === computer) {
+        //computer > player;
+        h6.innerHTML = "Computer Wins this Round";
+    }
+}
+
+// render controls (btns)
+function controlBtns() {
+
+}
+
+// handle player move -- use extraction to grab idx of a sq
+// ---Q: i feel like im missing something important that makes 
+function playersMove(choice) {
+    const idx = parseInt(choice.target.id.replace('sq-', ''));
+    if (board[idx] || isNaN[idx] || winner)
+        return;
+    else if (turn) {
+        turn *= -1;
+        winner = findWinner();
+        render();
+    }
+}
+
+
+
+//handle computer move
+//-----randomized function looking for sq values of null to choose randomly until winner combo or tie game (no more null tiles)
+function computersMove() { };
+//code to computers turn with randomized function
+
+//   else if (turn) {
+//     turn *= -1;
+//     winner = findWinner();
+//     render();
 // }
+
+
+// findWinner functions (horiz, vertical and diag)
+function findWinner() {
+    for (let i = 0; i < winningCombos.length; i++) {
+        if (Math.abs(board[0] + board[1] + board[2]) === 3) return board[0];
+        if (Math.abs(board[3] + board[4] + board[5]) === 3) return board[3];
+        if (Math.abs(board[6] + board[7] + board[8]) === 3) return board[6];
+        if (Math.abs(board[0] + board[3] + board[6]) === 3) return board[0];
+        if (Math.abs(board[1] + board[4] + board[7]) === 3) return board[1];
+        if (Math.abs(board[2] + board[5] + board[8]) === 3) return board[2];
+        if (Math.abs(board[0] + board[4] + board[8]) === 3) return board[0];
+        if (Math.abs(board[2] + board[4] + board[6]) === 3) return board[2];
+    } if (board.includes('')) return '';
+    return 'tie'
+}
+
+// best three of 5 freq counter 
